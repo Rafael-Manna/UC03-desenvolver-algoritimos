449c76817e795d1cc1c0cfd2be3eee971dbcfb7a
RAFAEL SANTOS MANNA <a95826722@rn.senac.local>
2025-09-23 21:14:39 -0300

    :sparkles:

---
+++ Atividade-12/Encap1.js
@@ -0,0 +1,55 @@
+import PromptSync from 'prompt-sync';
+const prompt = PromptSync();
+class Cliente{
+    #nome;
+    #endereco;
+    #renda;
+    constructor(nome = "",endereco = "", renda = ""){
+        this.#nome = nome
+        this.#endereco = endereco
+        this.#renda = renda
+    }
+    imprimir(){
+        console.log(` \nNome: ${this.#nome} \nEndereço: ${this.#endereco} \nRenda: ${this.#renda}`)
+      }
+    get nome(){
+        return this.#nome;
+    }
+    get endereco(){
+        return this.#endereco;
+    }
+    get renda(){
+        return this.#renda;
+    }
+    set nome(novoNome) {
+        if (typeof novoNome === "string" && novoNome.trim() !== "") {
+          this.#nome = novoNome;
+        } else {
+          console.log("Nome inválido");
+        }
+      }
+    
+      set endereco(novoEndereco) {
+        if (typeof novoEndereco === "string" && novoEndereco.trim() !== "") {
+          this.#endereco = novoEndereco;
+        } else {
+          console.log("Endereço inválido");
+        }
+      }
+    
+      set renda(novaRenda) {
+        if (typeof novaRenda === "number" && novaRenda >= 0) {
+          this.#renda = novaRenda;
+        } else {
+          console.log("Renda inválida");
+        }
+      }
+}
+let cliente1 = new Cliente("Julia","Casa do baralho",2.50);
+let cliente2 = new Cliente("Paulo","Rua asfaltada",2560.50);
+let cliente3 = new Cliente(prompt("Digite Seu nome: ") ,  (prompt("Digite seu endereço: ")) , parseFloat(prompt("Digite sua renda: ")))
+
+cliente1.imprimir();
+cliente2.imprimir();
+cliente3.imprimir();
+
