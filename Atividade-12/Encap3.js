ad6f7ae63dd82a58c52b7f67af1bde08ae7d6b54
RAFAEL SANTOS MANNA <a95826722@rn.senac.local>
2025-09-25 21:21:04 -0300

    :Sparkles:

---
+++ Atividade-12/Encap3.js
@@ -0,0 +1,149 @@
+import PromptSync from 'prompt-sync';
+const prompt = PromptSync();
+let usoram = 0
+let estadomonitor = "Desligado"
+class Memoria{
+    #ramtipo
+    #ramhz
+    #ramtam
+    constructor(ramtipo,ramhz,ramtam){
+    this.#ramtipo = ramtipo;
+    this.#ramhz = ramhz;
+    this.#ramtam = ramtam;
+    }
+    get ramtipo(){
+        return this.#ramtipo;
+    }
+    get ramhz(){
+        return this.#ramhz;
+    }
+    get ramtam(){
+        return this.#ramtam;
+    }
+    set ramtipo(novoramtipo){
+        this.#ramtipo = novoramtipo;
+    }
+    set ramhz(novoramhz){
+        this.#ramhz = novoramhz;
+    }
+    set ramtam(novoramtam){
+        this.#ramtam = novoramtam;
+    }
+    usarMemoria(qtd){
+      if((usoram + qtd )<=this.#ramtam ){
+        usoram = usoram + qtd;
+        console.log(`Uso atual de ram: ${usoram}`)
+      } 
+      else{
+        console.log("Uso de ram esta cheio.")
+      }
+    }
+    liberarMemoria(qtd){
+            usoram += -qtd;
+            if(usoram < 0){
+                usoram = 0;        
+     }
+     console.log(`Uso atual de ram: ${usoram}`)
+    }
+}
+class Processador{
+    #cpumarca
+    #cpumodelo
+    #cpunucleo
+    #cpufreq
+    constructor(cpumarca,cpumodelo,cpunucleo,cpufreq){
+    this.#cpumarca = cpumarca
+    this.#cpumodelo = cpumodelo
+    this.#cpunucleo = cpunucleo
+    this.#cpufreq = cpufreq
+    }
+    get cpumarca(){
+        return this.#cpumarca;
+    }
+    get cpumodelo(){
+        return this.#cpumodelo;
+    }
+    get cpunucleo(){
+        return this.#cpunucleo;
+    }
+    get cpufreq(){
+        return this.#cpufreq;
+    }
+    set cpumarca(novocpumarca){
+         this.#cpumarca = novocpumarca;
+    }
+    set cpumodelo(novocpumodelo){
+         this.#cpumodelo = novocpumodelo;
+    }
+    set cpunucleo(novocpunucleo){
+         this.#cpunucleo = novocpunucleo;
+    }
+    set cpufreq(novocpufreq){
+         this.#cpufreq = novocpufreq;
+    }
+}
+class Armazenamento{
+    #tipostorage
+    #tamstorage
+    #usostorage
+    constructor(tipoestorage,tamstorage,usostorage){
+        this.#tipostorage = tipoestorage
+        this.#tamstorage = tamstorage
+        this.#usostorage = 0
+    }
+    usarStorage(qtd){
+        if((this.usostorage + qtd )<=this.#tamstorage ){
+          this.usostorage +=  qtd;
+          console.log(`Uso atual de armazenamento: ${this.usarStorage}`)
+        } 
+        else{
+          console.log("Armazenamento CHEIO.")
+        }
+      }
+
+}
+class Monitor {
+    #resolucao
+    #tammonitor
+    constructor(resolucao,tammonitor){
+        this.#resolucao = resolucao
+        this.#tammonitor = tammonitor
+    }
+    get resolucao(){
+        return this.#resolucao;
+    }
+    get tammonitor(){
+        return this.#tammonitor;
+    }
+    set resolucao(novoresolucao){
+         this.#resolucao = novoresolucao;
+    }
+    set tammonitor(novotammonitor){
+        this.#tammonitor = novotammonitor;
+   }
+   ligdes(resposta){
+    if (respota == 1){
+        estadomonitor = "Ligado"
+    }
+    else{
+         estadomonitor = "Desligado"
+    }
+   }
+}
+class Computador{
+    
+}
+let memoria1 = new Memoria();
+memoria1.ramtam = 16;
+console.log("RAM+?")
+memoria1.usarMemoria(Number(prompt()));
+console.log("RAM+?")
+memoria1.usarMemoria(Number(prompt()));
+console.log("RAM+?")
+memoria1.usarMemoria(Number(prompt()));
+console.log("RAM-?")
+memoria1.liberarMemoria(Number(prompt()));
+console.log("RAM-?")
+memoria1.liberarMemoria(Number(prompt()));
+console.log("RAM-?")
+memoria1.liberarMemoria(Number(prompt()));
