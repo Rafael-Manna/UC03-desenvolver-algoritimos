0ed9394f9427e30f71ed788a980f89157d7fd0f1
Rafael-Manna <rafaelmannager@gmail.com>
2025-10-13 21:16:06 -0300

    :P

--- Atividade-14/arrayobj.js
+++ Atividade-14/arrayobj.js
@@ -1,7 +1,43 @@
 import PromptSync from 'prompt-sync';
 const prompt = PromptSync();
 class Hotel{
-    
+  constructor(){
+    this.nomeH = "Hotel"
+    this.listaDeQuartos = [];
+    this.listaClientes = [];
+  }
+  adicionarCliente() {
+    let nome = prompt("Digite o nome do cliente:");
+    let cpf = Number(prompt("Digite o CPF:"));
+    let contato = prompt("Digite o contato:");
+
+    let novoCliente = new Cliente(nome, cpf, contato);
+    this.listaDeClientes.push(novoCliente);
+    console.log("Cliente cadastrado com sucesso!");
+  }
+  adicionarQuarto(){
+    let adcnum = Number( prompt ("Digite o numero do Quarto: "))
+    let adctipo = prompt("Digite o tipo do Quarto: ")
+    for(let i = 0; i < this.listaDeQuartos.length;i++){
+      if (adcnum == this.listaDeQuartos[i].numeroQuarto){
+        console.log("Quarto ja existe!!");
+        return;
+      }
+    }
+    let novoQuarto = new Quarto(adcnum,adctipo);
+    this.listaDeQuartos.push(novoQuarto);
+  }
+  reservarQuarto(){
+    let adccli = prompt ("Digite o nome do cliente:");
+    let adccpf = Number( prompt ("Digite o Cpf do cliente que quer reservar: "));
+    let adcctt = Number( prompt ("Digite o Numero do cliente que quer reservar: "));
+    console.log(this.listaDeQuartos);
+    let adcnumr = Number( prompt ("Digite o numero do Quarto: "));
+
+  }
+  listarQuartos(){
+    console.log(this.listaDeQuartos);
+  }
 }
 class Cliente {
   // Campos privados
@@ -10,8 +46,10 @@ class Cliente {
 
   constructor(nome = "", cpf = "", contato = "") {
     this.nome = nome;       
-    this.#cpf = cpf;
+    this.#cpf = Number(cpf);
     this.#contato = contato;
+    this.listaDeClientes = [];
+
   }
 
   imprimir() {
@@ -23,8 +61,7 @@ Contato: ${this.#contato}
 -------------------------------
     `);
   }
-
-
+  
   get cpf() {
     return this.#cpf;
   }
@@ -43,15 +80,15 @@ Contato: ${this.#contato}
 
 }
 class Quarto {
-    constructor(numero, tipoQuarto) {
-        this.numero = numero;
+    constructor(numeroQuarto, tipoQuarto) {
+        this.numeroQuarto = numeroQuarto;
         this.tipoQuarto = tipoQuarto;
     }
 
     imprimir() {
         console.log(`
 ------ DADOS DO QUARTO ------
-Número do Quarto: ${this.numero}
+Número do Quarto: ${this.numeroQuarto}
 Tipo de Quarto: ${this.tipoQuarto}
 -------------------------------
 `);
@@ -71,8 +108,16 @@ class Reserva {
     console.log(`Data da reserva: ${this.dataReserva}`);
   }
 }
-const cliente1 = new Cliente("Rafael", "123.456.789-00", "11 99999-9999");
+
+const cliente1 = new Cliente();
+cliente1.adicionarCliente();
 const quarto1 = new Quarto(101, "Suíte Deluxe");
 const reserva1 = new Reserva(cliente1, quarto1, "13/10/2025");
 
 reserva1.imprimir();
+const meuHotel = new Hotel();
+meuHotel.adicionarQuarto();
+meuHotel.adicionarQuarto();
+meuHotel.adicionarQuarto();
+
+meuHotel.listarQuartos();
